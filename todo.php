94a258ade63d995021feb97d9e85101f226d4c55
Vladislav Chernikov <vlad.chernikov.2002@bk.ru>
2023-11-01 19:11:12 +0200

    added report command

--- todo.php
+++ todo.php
@@ -38,6 +38,9 @@ function main(array $arguments): void
 		case 'rm':
 			removeCommand($arguments);
 			break;
+		case 'report':
+			reportCommand($arguments);
+			break;
 
 		default:
 			echo 'Unknown command';
